eea8ad76857ebf98bc611bfc5aabcd9875af4d6d
Paul Elliott <paul@codingfrontier.com>
2013-08-17 21:28:19 -0400

    Line up whitespace in InformantRails module

--- lib/informant_rails.rb
+++ lib/informant_rails.rb
@@ -1,8 +1,8 @@
 require 'informant_rails/railtie'
 
 module InformantRails
-  autoload :Config, 'informant_rails/config'
-  autoload :Client, 'informant_rails/client'
+  autoload :Config,     'informant_rails/config'
+  autoload :Client,     'informant_rails/client'
   autoload :Middleware, 'informant_rails/middleware'
-  autoload :Request, 'informant_rails/request'
+  autoload :Request,    'informant_rails/request'
 end
